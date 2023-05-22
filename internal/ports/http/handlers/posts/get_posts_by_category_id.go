ab547be14039109b68513b2250387ad950da526a
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2023-05-22 21:28:25 +0600

    adds get posts by category handler

---
+++ internal/ports/http/handlers/posts/get_posts_by_category_id.go
@@ -0,0 +1,37 @@
+package posts
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/DarkhanShakhan/forum-moderation/internal/util"
+)
+
+func (c *controller) getPostsByCategoryID(w http.ResponseWriter, r *http.Request) {
+	var req getPostsByCategoryIDRequest
+	if err := util.GetData(r, &req); err != nil {
+		util.SendError(w, err, http.StatusBadRequest)
+		return
+	}
+	posts, err := c.postsService.GetPostsByCategory(r.Context(), req.ID)
+	if err != nil {
+		return
+	}
+	util.SendData(w, http.StatusOK, util.NewSuccessResponse(newPostsResponse(posts)))
+}
+
+type getPostsByCategoryIDRequest struct {
+	ID int64
+}
+
+func (r *getPostsByCategoryIDRequest) GetParams(req *http.Request) error {
+	//FIXME: regex
+	path := strings.Split(req.URL.Path, "/")
+	id, err := strconv.Atoi(path[len(path)-1])
+	if err != nil {
+		return err
+	}
+	r.ID = int64(id)
+	return nil
+}
